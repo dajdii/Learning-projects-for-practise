22f221b28ba710e054a5911df4e22397d3cdde28
David Dang <dangdavid@outlook.cz>
2022-11-19 10:37:06 +0100

    Add files via upload

---
+++ Paint Job/src/Main.java
@@ -0,0 +1,62 @@
+public class Main {
+    public static void main(String[] args) {
+        // 3#
+        System.out.println("Hello world!");
+        System.out.println(getBucketCount(3.4, 1.5));
+        System.out.println(getBucketCount(6.26, 2.2));
+        System.out.println(getBucketCount(3.26, 0.75));
+
+        // 2#
+        System.out.println(getBucketCount(-3.4, 2.1, 1.5));
+        System.out.println(getBucketCount(3.4, 2.1, 1.5));
+        System.out.println(getBucketCount(7.25, 4.3, 2.35));
+
+        // 1#
+        System.out.println(getBucketCount(-3.4, 2.1, 1.5, 2));
+        System.out.println(getBucketCount(3.4, 2.1, 1.5, 2));
+        System.out.println(getBucketCount(2.75, 3.25, 2.5, 1));
+
+
+    }
+
+    public static int getBucketCount(double width, double height, double areaPerBucket, int extraBuckets){
+        if ((width <= 0 || height <= 0 || areaPerBucket <= 0) || extraBuckets < 0){
+            return -1;
+        }
+
+        double area = width * height;
+        double countOfBucket = 0;
+        if (area % areaPerBucket > 0){
+            countOfBucket = (area / areaPerBucket) + 1 - (double)extraBuckets;
+        }
+        return (int)countOfBucket;
+
+    }
+
+    public static int getBucketCount(double width, double height, double areaPerBucket){
+        if ((width <= 0 || height <= 0 || areaPerBucket <= 0)){
+            return -1;
+        }
+
+        double area = width * height;
+        double countOfBucket = 0;
+        if (area % areaPerBucket > 0){
+            countOfBucket = (area / areaPerBucket) + 1;
+        }
+        return (int)countOfBucket;
+
+    }
+
+    public static int getBucketCount(double area, double areaPerBucket){
+        if (area<= 0 || areaPerBucket <= 0){
+            return -1;
+        }
+
+        double countOfBucket = 0;
+        if (area % areaPerBucket > 0){
+            countOfBucket = (area / areaPerBucket) + 1;
+        }
+        return (int)countOfBucket;
+
+    }
+}
