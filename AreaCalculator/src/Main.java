91df99a850fa80e9431ffad83c882778bc467c42
David Dang <dangdavid@outlook.cz>
2022-11-19 10:32:27 +0100

    Add files via upload

---
+++ AreaCalculator/src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //code here
+        System.out.println(area(5.0));
+        System.out.println(area(5.0, 4.0));
+
+    }
+
+    public static double area(double radius){
+        if (radius < 0){
+            return -1.0;
+        }
+
+        return radius * radius * Math.PI;
+    }
+
+    public static double area(double x, double y){
+        if ((x < 0) || (y < 0)){
+            return -1.0;
+        }
+
+        return x * y;
+    }
+}
